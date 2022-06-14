a3d840e302fe30d7a921847458a084e1989f0fbd
vickyruud <aprajit.k@gmail.com>
2022-06-14 14:30:25 -0400

    Add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
 import Skills from "./components/Skills"
@@ -13,7 +14,8 @@ function App() {
       <Home />
       <About />
       <Skills />
-      <Work/>
+      <Work />
+      <Contact/>
     </div>
   )
 }
