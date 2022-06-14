51662f45a42fd485f10b9f16d7380c184bbe53fb
vickyruud <aprajit.k@gmail.com>
2022-06-14 14:13:21 -0400

    finish skills/experience and add work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import About from "./components/About"
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
+import Skills from "./components/Skills"
+import Work from "./components/Work"
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Skills />
+      <Work/>
     </div>
   )
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div>
+        <div>
+          <p>
+            
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Work
