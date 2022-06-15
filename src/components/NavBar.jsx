9d2c7f4f6c6d56e66b7c940d8d58a46e265d7c5f
vickyruud <aprajit.k@gmail.com>
2022-06-15 19:12:18 -0400

    change logo font

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import Logo from "../assets/logo2.png";
+import Logo from "../assets/logo.png";
 import { Link } from "react-scroll";
 
 const NavBar = () => {
