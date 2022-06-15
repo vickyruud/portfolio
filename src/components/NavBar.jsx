7ccba6934f62a37c19eb99ca19388887a732a3fd
vickyruud <aprajit.k@gmail.com>
2022-06-15 16:52:18 -0400

    add socials to mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -82,6 +82,38 @@ const NavBar = () => {
             Contact
           </Link>
         </li>
+        <li className="w-[160px] h-[60px] flex justify-between items-center  bg-blue-800">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/aprajitk/"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center  bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/vickyruud"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center  bg-[#6fc2b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:aprajit.k@gmail.com"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center  bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
       </ul>
       {/* Social Icons */}
       <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
