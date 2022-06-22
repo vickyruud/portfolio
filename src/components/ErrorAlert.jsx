1bd81572fa12f443ae10afc7d56873a819e264da
vickyruud <aprajit.k@gmail.com>
2022-06-21 21:43:21 -0400

    change error message style

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ErrorAlert = ({message, handleClose}) => {
   return (
- <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 m-4 flex flex-row justify-between" role="alert">
-  <p className="font-bold">Warning!</p>
+ <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-600 p-4 m-4 flex flex-row justify-between" role="alert">
+  <p className="font-semibold">Error!</p>
       <p className='pl-2'>{message}</p>
 <button onClick={handleClose} className='pl-4'>x</button>
 </div>
